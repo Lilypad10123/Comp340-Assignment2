381d4ca1e5fe3fd58b79c74e90dbe539436f0631
Lilypad10123 <julia.wosnack@student.ufv.ca>
2021-10-18 15:22:45 -0700

    Start parent.c file

--- parent.c
+++ parent.c
@@ -1,1 +1,25 @@
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "child.c"
 
+
+void *pause(void*);
+
+int main() {
+	pthread_t tid;
+	pthread_create(&tid, NULL, pause, NULL);
+	pthread_join(tid, NULL);
+
+	return EXIT_SUCCESS;
+}
+
+void *pause(void *args) {
+	struct timespec ts;
+	ts.tv_sec = 0;
+	ts.tv_nsec = 100000;
+	while {
+		nanosleep(&ts, NULL);
+	}
+}
